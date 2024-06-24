47eb024d7bcf10c2a14e5ddc24065d60cb58654c
Dashboy1998 <18587674+Dashboy1998@users.noreply.github.com>
2024-06-23 21:10:36 -0400

    Removed last continue

--- rename_shows.sh
+++ rename_shows.sh
@@ -54,57 +54,56 @@ for ((i = 0 ; i < $metadata_all_length ; i++ )); do
             while read -r id; do
                 get_tmdbid
             done < <(printf "$guid" | jq --raw-output '.[].id')
+
+            # Get year
+            year=$( printf "$metadata" | jq --raw-output '.[0].year' )
+            if ! [ "$year" == "null" ]; then
+                # Get paths
+                media=$( printf "$metadata" | jq '.[0].Location' )
+                length=$( printf "$media" | jq length )
+                if [ $length -eq 1 ]; then
+                    # Check length of parts
+                    part=$( printf "$media" | jq --raw-output '.[0].path' )
+
+                        file_full_path=$( printf "$part" | sed "s|$plex_media_path|$media_path|")
+                        file_path=$( dirname "$file_full_path" )
+                        file_name=$( basename "$file_full_path" )
+                        directory_name=$( dirname "$file_path" )
+                        if ! [[ "${directory_name,,}" == "${file_path}" ]]; then
+                            file_new_name="$file_name"
+                            # Check if year exists in file name
+                            file_year=$( echo "$file_name" | grep -o -P '(?<=\()[0-9]{4}(?=\))' || true)
+                            if [ -z "$file_year" ] || [[ "$file_year" =~ ^[0-9]{4}$ ]]; then
+                                if [ -z "$file_year" ] || [ "$year" == "$file_year" ]; then
+                                    # Check if year exists in file name
+                                    if [[ "$file_name" != *"($year)"* ]]; then
+                                        file_new_name="$file_new_name ($year)"
+                                    fi
+
+                                    # Check if TMDBID exists in file name
+                                    # TODO Verify another TMDBID does not exist
+                                    if [[ "$file_name" != *"[tmdbid-$tmdbid]"* ]]; then
+                                        file_new_name="$file_new_name [tmdbid-$tmdbid]"
+                                    fi
+                                    file_new_name="$file_new_name"
+                                    rename_item "$file_new_name"
+                                else
+                                    echoerr "Different year in metadata than in filename"
+                                fi
+                            elif ! [[ "$file_year" =~ ^[0-9]{4}$ ]]; then
+                                echoerr "Multiple Years detected for $plex_rating_key: $title"
+                            fi
+                        else
+                            echoerr "Need to rename dir: $file_path"
+                        fi
+                else
+                    echoerr "Media length is not 1. Media length is $length for $plex_rating_key: $title"
+                fi 
+            else
+                echoerr "Year is null for $plex_rating_key: $title"
+            fi
         else
             echoerr "GUID does not exist for $plex_rating_key: $title"
-            continue
-        fi
-
-        # Get year
-        year=$( printf "$metadata" | jq --raw-output '.[0].year' )
-        if ! [ "$year" == "null" ]; then
-            # Get paths
-            media=$( printf "$metadata" | jq '.[0].Location' )
-            length=$( printf "$media" | jq length )
-            if [ $length -eq 1 ]; then
-                # Check length of parts
-                part=$( printf "$media" | jq --raw-output '.[0].path' )
-
-                    file_full_path=$( printf "$part" | sed "s|$plex_media_path|$media_path|")
-                    file_path=$( dirname "$file_full_path" )
-                    file_name=$( basename "$file_full_path" )
-                    directory_name=$( dirname "$file_path" )
-                    if ! [[ "${directory_name,,}" == "${file_path}" ]]; then
-                        file_new_name="$file_name"
-                        # Check if year exists in file name
-                        file_year=$( echo "$file_name" | grep -o -P '(?<=\()[0-9]{4}(?=\))' || true)
-                        if [ -z "$file_year" ] || [[ "$file_year" =~ ^[0-9]{4}$ ]]; then
-                            if [ -z "$file_year" ] || [ "$year" == "$file_year" ]; then
-                                # Check if year exists in file name
-                                if [[ "$file_name" != *"($year)"* ]]; then
-                                    file_new_name="$file_new_name ($year)"
-                                fi
-
-                                # Check if TMDBID exists in file name
-                                # TODO Verify another TMDBID does not exist
-                                if [[ "$file_name" != *"[tmdbid-$tmdbid]"* ]]; then
-                                    file_new_name="$file_new_name [tmdbid-$tmdbid]"
-                                fi
-                                file_new_name="$file_new_name"
-                                rename_item "$file_new_name"
-                            else
-                                echoerr "Different year in metadata than in filename"
-                            fi
-                        elif ! [[ "$file_year" =~ ^[0-9]{4}$ ]]; then
-                            echoerr "Multiple Years detected for $plex_rating_key: $title"
-                        fi
-                    else
-                        echoerr "Need to rename dir: $file_path"
-                    fi
-            else
-                echoerr "Media length is not 1. Media length is $length for $plex_rating_key: $title"
-            fi 
-        else
-            echoerr "Year is null for $plex_rating_key: $title"
         fi
     else
         echoerr "Length of Metadata is greater not 1. Length is $length for $plex_rating_key"
