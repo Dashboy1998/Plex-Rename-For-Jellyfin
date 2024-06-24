ef5b53cade4d6357f835842a5ee122a0f7d6de4d
Dashboy1998 <18587674+Dashboy1998@users.noreply.github.com>
2024-06-23 21:18:09 -0400

    Added function to process library

--- rename_movies.sh
+++ rename_movies.sh
@@ -30,90 +30,95 @@ function rename_item(){
   fi
 }
 
-metadata_all=$( curl --silent --request GET \
-  --url "$plex_server_protocol://$plex_server_address:$plex_server_port/library/sections/$plex_section_id/$plex_section_tag" \
-  --header "X-Plex-Token: $x_plex_token" \
-  --header "Accept: application/json" | jq '[.MediaContainer.Metadata[] | {ratingKey}]' )
-
-metadata_all_length=$( printf "$metadata_all" | jq length )
-
-# Get metadata for item
-for ((i = 0 ; i < $metadata_all_length ; i++ )); do
-  plex_rating_key=$( printf "$metadata_all" | jq --raw-output ".[$i].ratingKey" )
-  metadata=$( curl --silent --request GET \
-    --url "$plex_server_protocol://$plex_server_address:$plex_server_port/library/metadata/$plex_rating_key" \
+function process_library(){
+  local plex_section_id=$1
+  metadata_all=$( curl --silent --request GET \
+    --url "$plex_server_protocol://$plex_server_address:$plex_server_port/library/sections/$plex_section_id/$plex_section_tag" \
     --header "X-Plex-Token: $x_plex_token" \
-    --header "Accept: application/json" | jq '[.MediaContainer.Metadata[] | {year, Guid, Media, title}]' )
+    --header "Accept: application/json" | jq '[.MediaContainer.Metadata[] | {ratingKey}]' )
 
-  length=$( printf "$metadata" | jq length )
-  if [ $length -eq 1 ]; then
-    title=$( printf "$metadata" | jq --raw-output '.[0].title'  )
-    # Check if Guids exists
-    guid=$( printf "$metadata" | jq --raw-output '.[0].Guid' )
-    if [ "$guid" != "null" ]; then
-      # Iterate over Guids
-      while read -r id; do
-        get_tmdbid
-      done < <(printf "$guid" | jq --raw-output '.[].id')
+  metadata_all_length=$( printf "$metadata_all" | jq length )
 
-      # Get year
-      year=$( printf "$metadata" | jq --raw-output '.[0].year' )
-      if ! [ "$year" == "null" ]; then
-        # Get paths
-        media=$( printf "$metadata" | jq '.[0].Media' )
-        length=$( printf "$media" | jq length )
-        if [ $length -eq 1 ]; then
-          # Check length of parts
-          part=$( printf "$media" | jq '.[0].Part' )
-          length=$( printf "$part" | jq length )
+  # Get metadata for item
+  for ((i = 0 ; i < $metadata_all_length ; i++ )); do
+    plex_rating_key=$( printf "$metadata_all" | jq --raw-output ".[$i].ratingKey" )
+    metadata=$( curl --silent --request GET \
+      --url "$plex_server_protocol://$plex_server_address:$plex_server_port/library/metadata/$plex_rating_key" \
+      --header "X-Plex-Token: $x_plex_token" \
+      --header "Accept: application/json" | jq '[.MediaContainer.Metadata[] | {year, Guid, Media, title}]' )
+
+    length=$( printf "$metadata" | jq length )
+    if [ $length -eq 1 ]; then
+      title=$( printf "$metadata" | jq --raw-output '.[0].title'  )
+      # Check if Guids exists
+      guid=$( printf "$metadata" | jq --raw-output '.[0].Guid' )
+      if [ "$guid" != "null" ]; then
+        # Iterate over Guids
+        while read -r id; do
+          get_tmdbid
+        done < <(printf "$guid" | jq --raw-output '.[].id')
+
+        # Get year
+        year=$( printf "$metadata" | jq --raw-output '.[0].year' )
+        if ! [ "$year" == "null" ]; then
+          # Get paths
+          media=$( printf "$metadata" | jq '.[0].Media' )
+          length=$( printf "$media" | jq length )
           if [ $length -eq 1 ]; then
-            file_full_path=$( printf "$part" | jq --raw-output '.[0].file' | sed "s|$plex_media_path|$media_path|")
-            file_path=$( dirname "$file_full_path" )
-            file_name=$( basename "$file_full_path" )
-            file_ext=${file_name##*.}
-            file_name="${file_name%.*}"
+            # Check length of parts
+            part=$( printf "$media" | jq '.[0].Part' )
+            length=$( printf "$part" | jq length )
+            if [ $length -eq 1 ]; then
+              file_full_path=$( printf "$part" | jq --raw-output '.[0].file' | sed "s|$plex_media_path|$media_path|")
+              file_path=$( dirname "$file_full_path" )
+              file_name=$( basename "$file_full_path" )
+              file_ext=${file_name##*.}
+              file_name="${file_name%.*}"
 
-            directory_name=$( dirname "$file_path" )
-            if ! [[ "${directory_name,,}" == "${file_path}" ]]; then
-              file_new_name="$file_name"
-              # Check if year exists in file name
-              file_year=$( echo "$file_name" | grep -o -P '(?<=\()[0-9]{4}(?=\))' || true)
-              if [ -z "$file_year" ] || [[ "$file_year" =~ ^[0-9]{4}$ ]]; then
-                if [ -z "$file_year" ] || [ "$year" == "$file_year" ]; then
-                  # Check if file name year matches
-                  if [[ "$file_name" != *"($year)"* ]]; then
-                    file_new_name="$file_new_name ($year)"
-                  fi
+              directory_name=$( dirname "$file_path" )
+              if ! [[ "${directory_name,,}" == "${file_path}" ]]; then
+                file_new_name="$file_name"
+                # Check if year exists in file name
+                file_year=$( echo "$file_name" | grep -o -P '(?<=\()[0-9]{4}(?=\))' || true)
+                if [ -z "$file_year" ] || [[ "$file_year" =~ ^[0-9]{4}$ ]]; then
+                  if [ -z "$file_year" ] || [ "$year" == "$file_year" ]; then
+                    # Check if file name year matches
+                    if [[ "$file_name" != *"($year)"* ]]; then
+                      file_new_name="$file_new_name ($year)"
+                    fi
 
-                  # Check if TMDBID exists in file name
-                  # TODO Verify another TMDBID does not exist
-                  if [[ "$file_name" != *"[tmdbid-$tmdbid]"* ]]; then
-                    file_new_name="$file_new_name [tmdbid-$tmdbid]"
+                    # Check if TMDBID exists in file name
+                    # TODO Verify another TMDBID does not exist
+                    if [[ "$file_name" != *"[tmdbid-$tmdbid]"* ]]; then
+                      file_new_name="$file_new_name [tmdbid-$tmdbid]"
+                    fi
+                    file_new_name="$file_new_name.$file_ext"
+                    rename_item "$file_new_name"
+                  else
+                    echoerr "Different year in metadata than in filename"
                   fi
-                  file_new_name="$file_new_name.$file_ext"
-                  rename_item "$file_new_name"
                 else
-                  echoerr "Different year in metadata than in filename"
+                  echoerr "Multiple Years detected for $plex_rating_key: $title"
                 fi
               else
-                echoerr "Multiple Years detected for $plex_rating_key: $title"
+                echoerr "Need to rename dir: $file_path"
               fi
             else
-              echoerr "Need to rename dir: $file_path"
+              echoerr "Part length is not 1. Part length is $length for $plex_rating_key: $title"
             fi
           else
-            echoerr "Part length is not 1. Part length is $length for $plex_rating_key: $title"
-          fi
+            echoerr "Media length is not 1. Media length is $length for $plex_rating_key: $title"
+          fi 
         else
-          echoerr "Media length is not 1. Media length is $length for $plex_rating_key: $title"
-        fi 
+          echoerr "Year is null for $plex_rating_key: $title"
+        fi
       else
-        echoerr "Year is null for $plex_rating_key: $title"
+        echoerr "GUID does not exist for $plex_rating_key: $title"
       fi
     else
-      echoerr "GUID does not exist for $plex_rating_key: $title"
+      echoerr "Length of Metadata is greater not 1. Length is $length for $plex_rating_key"
     fi
-  else
-    echoerr "Length of Metadata is greater not 1. Length is $length for $plex_rating_key"
-  fi
-done
+  done
+}
+
+process_library $plex_section_id
--- rename_shows.sh
+++ rename_shows.sh
@@ -30,82 +30,87 @@ function rename_item(){
   fi
 }
 
-metadata_all=$( curl --silent --request GET \
-  --url "$plex_server_protocol://$plex_server_address:$plex_server_port/library/sections/$plex_section_id/$plex_section_tag" \
-  --header "X-Plex-Token: $x_plex_token" \
-  --header "Accept: application/json" | jq '[.MediaContainer.Metadata[] | {ratingKey}]' )
-metadata_all_length=$( printf "$metadata_all" | jq length )
-
-# Get metadata for item
-for ((i = 0 ; i < $metadata_all_length ; i++ )); do
-  plex_rating_key=$( printf "$metadata_all" | jq --raw-output ".[$i].ratingKey" )
-  metadata=$( curl --silent --request GET \
-    --url "$plex_server_protocol://$plex_server_address:$plex_server_port/library/metadata/$plex_rating_key" \
+function process_library(){
+  local plex_section_id=$1
+  metadata_all=$( curl --silent --request GET \
+    --url "$plex_server_protocol://$plex_server_address:$plex_server_port/library/sections/$plex_section_id/$plex_section_tag" \
     --header "X-Plex-Token: $x_plex_token" \
-    --header "Accept: application/json" | jq '[.MediaContainer.Metadata[] | {year, Guid, Location, title}]' )
+    --header "Accept: application/json" | jq '[.MediaContainer.Metadata[] | {ratingKey}]' )
+  metadata_all_length=$( printf "$metadata_all" | jq length )
 
-  length=$( printf "$metadata" | jq length )
-  if [ $length -eq 1 ]; then
-    title=$( printf "$metadata" | jq --raw-output '.[0].title'  )
-    # Check if Guids exists
-    guid=$( printf "$metadata" | jq --raw-output '.[0].Guid' )
-    if [ "$guid" != "null" ]; then
-      # Iterate over Guids
-      while read -r id; do
-        get_tmdbid
-      done < <(printf "$guid" | jq --raw-output '.[].id')
+  # Get metadata for item
+  for ((i = 0 ; i < $metadata_all_length ; i++ )); do
+    plex_rating_key=$( printf "$metadata_all" | jq --raw-output ".[$i].ratingKey" )
+    metadata=$( curl --silent --request GET \
+      --url "$plex_server_protocol://$plex_server_address:$plex_server_port/library/metadata/$plex_rating_key" \
+      --header "X-Plex-Token: $x_plex_token" \
+      --header "Accept: application/json" | jq '[.MediaContainer.Metadata[] | {year, Guid, Location, title}]' )
 
-      # Get year
-      year=$( printf "$metadata" | jq --raw-output '.[0].year' )
-      if ! [ "$year" == "null" ]; then
-        # Get paths
-        media=$( printf "$metadata" | jq '.[0].Location' )
-        length=$( printf "$media" | jq length )
-        if [ $length -eq 1 ]; then
-          # Check length of parts
-          part=$( printf "$media" | jq --raw-output '.[0].path' )
+    length=$( printf "$metadata" | jq length )
+    if [ $length -eq 1 ]; then
+      title=$( printf "$metadata" | jq --raw-output '.[0].title'  )
+      # Check if Guids exists
+      guid=$( printf "$metadata" | jq --raw-output '.[0].Guid' )
+      if [ "$guid" != "null" ]; then
+        # Iterate over Guids
+        while read -r id; do
+          get_tmdbid
+        done < <(printf "$guid" | jq --raw-output '.[].id')
 
-            file_full_path=$( printf "$part" | sed "s|$plex_media_path|$media_path|")
-            file_path=$( dirname "$file_full_path" )
-            file_name=$( basename "$file_full_path" )
-            directory_name=$( dirname "$file_path" )
-            if ! [[ "${directory_name,,}" == "${file_path}" ]]; then
-              file_new_name="$file_name"
-              # Check if year exists in file name
-              file_year=$( echo "$file_name" | grep -o -P '(?<=\()[0-9]{4}(?=\))' || true)
-              if [ -z "$file_year" ] || [[ "$file_year" =~ ^[0-9]{4}$ ]]; then
-                if [ -z "$file_year" ] || [ "$year" == "$file_year" ]; then
-                  # Check if year exists in file name
-                  if [[ "$file_name" != *"($year)"* ]]; then
-                    file_new_name="$file_new_name ($year)"
+        # Get year
+        year=$( printf "$metadata" | jq --raw-output '.[0].year' )
+        if ! [ "$year" == "null" ]; then
+          # Get paths
+          media=$( printf "$metadata" | jq '.[0].Location' )
+          length=$( printf "$media" | jq length )
+          if [ $length -eq 1 ]; then
+            # Check length of parts
+            part=$( printf "$media" | jq --raw-output '.[0].path' )
+
+              file_full_path=$( printf "$part" | sed "s|$plex_media_path|$media_path|")
+              file_path=$( dirname "$file_full_path" )
+              file_name=$( basename "$file_full_path" )
+              directory_name=$( dirname "$file_path" )
+              if ! [[ "${directory_name,,}" == "${file_path}" ]]; then
+                file_new_name="$file_name"
+                # Check if year exists in file name
+                file_year=$( echo "$file_name" | grep -o -P '(?<=\()[0-9]{4}(?=\))' || true)
+                if [ -z "$file_year" ] || [[ "$file_year" =~ ^[0-9]{4}$ ]]; then
+                  if [ -z "$file_year" ] || [ "$year" == "$file_year" ]; then
+                    # Check if year exists in file name
+                    if [[ "$file_name" != *"($year)"* ]]; then
+                      file_new_name="$file_new_name ($year)"
+                    fi
+
+                    # Check if TMDBID exists in file name
+                    # TODO Verify another TMDBID does not exist
+                    if [[ "$file_name" != *"[tmdbid-$tmdbid]"* ]]; then
+                      file_new_name="$file_new_name [tmdbid-$tmdbid]"
+                    fi
+                    file_new_name="$file_new_name"
+                    rename_item "$file_new_name"
+                  else
+                    echoerr "Different year in metadata than in filename"
                   fi
-
-                  # Check if TMDBID exists in file name
-                  # TODO Verify another TMDBID does not exist
-                  if [[ "$file_name" != *"[tmdbid-$tmdbid]"* ]]; then
-                    file_new_name="$file_new_name [tmdbid-$tmdbid]"
-                  fi
-                  file_new_name="$file_new_name"
-                  rename_item "$file_new_name"
-                else
-                  echoerr "Different year in metadata than in filename"
+                elif ! [[ "$file_year" =~ ^[0-9]{4}$ ]]; then
+                  echoerr "Multiple Years detected for $plex_rating_key: $title"
                 fi
-              elif ! [[ "$file_year" =~ ^[0-9]{4}$ ]]; then
-                echoerr "Multiple Years detected for $plex_rating_key: $title"
+              else
+                echoerr "Need to rename dir: $file_path"
               fi
-            else
-              echoerr "Need to rename dir: $file_path"
-            fi
+          else
+            echoerr "Media length is not 1. Media length is $length for $plex_rating_key: $title"
+          fi 
         else
-          echoerr "Media length is not 1. Media length is $length for $plex_rating_key: $title"
-        fi 
+          echoerr "Year is null for $plex_rating_key: $title"
+        fi
       else
-        echoerr "Year is null for $plex_rating_key: $title"
+        echoerr "GUID does not exist for $plex_rating_key: $title"
       fi
     else
-      echoerr "GUID does not exist for $plex_rating_key: $title"
+      echoerr "Length of Metadata is greater not 1. Length is $length for $plex_rating_key"
     fi
-  else
-    echoerr "Length of Metadata is greater not 1. Length is $length for $plex_rating_key"
-  fi
-done
+  done
+}
+
+process_library $plex_section_id
